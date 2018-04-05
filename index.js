c8534ee786a402974b4fe3859477a9dee182fc1c
Christopher Metzger <chrismetzgr@gmail.com>
2018-04-05 21:02:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
   return "YES INDEED!"
-  } else { 
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  else {
   return "I love you, too."} 
   }
